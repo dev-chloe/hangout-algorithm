e9ca54521fb5583ba29f10e269b5952a7d04d8bd
dev-chloe <chloekim.4969@gmail.com>
2022-09-15 19:10:03 +0900

    Lv1. 정수 내림차순으로 배치하기(풀이)

--- src/programmers/level1/descending-order-number/solution.js
+++ src/programmers/level1/descending-order-number/solution.js
@@ -1,6 +1,15 @@
 function solution(n) {
-  // insert here
-  return n;
+  // let answer = 0;
+  // // 정수 n 문자열로 변환
+  // const stringN = n.toString();
+  // // 문자열 n 자르기
+  // const splitN = stringN.split("");
+  // // 자른 문자열 n 내림차순 정렬
+  // const order = splitN.sort().reverse();
+  // // 내림차순 정렬된 문자 숫자로 바꿔 하나의 숫자로 합
+  // answer = parseInt(order.join(""));
+  // return answer;
+  return parseInt(n.toString().split("").sort().reverse().join(""));
 }
 
 export default solution;
