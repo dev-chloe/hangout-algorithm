506cc2472dd3d7527912c1b9476c7545f17c3f09
dev-chloe <chloekim.4969@gmail.com>
2022-09-17 16:12:19 +0900

    Lv1. 음양 더하기(풀이)

--- src/programmers/level1/sum-positive-negative/solution.js
+++ src/programmers/level1/sum-positive-negative/solution.js
@@ -1,6 +1,18 @@
-function solution(n) {
-  // insert here
-  return n;
+function solution(absolutes, signs) {
+  /** const arr = [];
+  // absolutes와 signs를 비교하여
+  for (let i = 0; i < absolutes.length; i++) {
+    // true 이면 양수
+    // false 이면 음수로 변환
+    signs[i] === true ? arr.push(absolutes[i]) : arr.push(-absolutes[i]);
+  }
+  // 모든 수를 합한다.
+  return arr.reduce((a, b) => a + b); **/
+  const arr = [];
+  absolutes.map((item, index) => {
+    arr.push(signs[index] ? item : -item);
+  });
+  return arr.reduce((a, b) => a + b);
 }
 
 export default solution;
