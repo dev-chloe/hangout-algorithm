6474601cea39d18661115345511ca5cc1dc6eb60
dev-chloe <chloekim.4969@gmail.com>
2022-09-17 17:02:47 +0900

    Lv1. 없는 숫자 더하기(풀이)

--- src/programmers/level1/sum-missing-number/solution.js
+++ src/programmers/level1/sum-missing-number/solution.js
@@ -1,6 +1,12 @@
 function solution(arr) {
-  // insert here
-  return arr;
+  // let answer = 0;
+  // // arr 안에 모든 숫자가 있는지 확인
+  // for (let i = 0; i <= 9; i++) {
+  //   if (!arr.includes(i)) { answer += i; }
+  // }
+  // // 없는 숫자를 더해준다.
+  // return answer;
+  return 45 - arr.reduce((acc, cur) => acc + cur, 0);
 }
 
 export default solution;
