ac968696efa8af53034ba3a1581121056335b333
dev-chloe <chloekim.4969@gmail.com>
2022-09-18 14:14:17 +0900

    Lv1. 행렬의 덧셈(풀이)

--- src/programmers/level1/sum-matrix/solution.js
+++ src/programmers/level1/sum-matrix/solution.js
@@ -1,6 +1,17 @@
 function solution(arr1, arr2) {
-  // insert here
-  return arr1, arr2;
+  /** const sumArr = [[]];
+  // arr1이 가진 배열을 확인
+  arr1.forEach((element, index) => {
+    sumArr[index] = [];
+    // arr1이 가진 배열의 요소를 확인
+    element.forEach((e, idx) => {
+      // 배열의 요소들과 arr2 배열의 요소를 합하여 반환
+      sumArr[index].push(e + arr2[index][idx]);
+    });
+  }); **/
+
+  const sumArr = arr1.map((o, index) => o.map((ele, idx) => ele + arr2[index][idx]));
+  return sumArr;
 }
 
 export default solution;
