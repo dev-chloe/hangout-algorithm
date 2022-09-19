031425874a85349f6b9458558f35e3a3057a2590
dev-chloe <chloekim.4969@gmail.com>
2022-09-19 12:51:03 +0900

    Lv1. 이상한 문자 만들기(풀이)

--- src/programmers/level1/make-strange-text/solution.js
+++ src/programmers/level1/make-strange-text/solution.js
@@ -1,6 +1,14 @@
-function solution(n) {
-  // insert here
-  return n;
+function solution(s) {
+  // 문자열 자르기
+  const splitS = s.split(" ");
+  // 빈 배열 생성
+  const answer = [];
+  for (const element of splitS) {
+    // 단어의 홀수 번째 글은 소문자 짝수는 대문자로 변환하여 배열에 넣는다
+    answer.push(element.split("").map((item, idx) => idx % 2 ? item.toLowerCase() : item.toUpperCase()).join(""));
+  }
+  // 문자로 변환
+  return answer.join(" ");
 }
 
 export default solution;
